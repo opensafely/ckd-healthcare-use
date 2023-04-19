8e81e321768d44a62278aba20010f9555f067584
Viyaasan Mahalingasivam <viyaasan@hotmail.com>
2023-04-19 13:51:38 +0100

    Prevalent CKD by stage for April 2023

--- analysis/study_definition_2023_ckd_complete_v2.py
+++ analysis/study_definition_2023_ckd_complete_v2.py
@@ -10,19 +10,19 @@ from cohortextractor import (
 
 from codelists import *
 
-CKD = "output/2022_ckd_v2.csv"
+CKD = "output/2023_ckd_v2.csv"
 
 from variables_additional_v2 import generate_variables_additional_v2
 variables_additional_v2= generate_variables_additional_v2(index_date_variable="index_date")
 
 study = StudyDefinition(
     default_expectations={
-        "date": {"earliest": "2022-04-01", "latest": "2023-03-31"},
+        "date": {"earliest": "2023-04-01", "latest": "2023-04-18"},
         "rate": "uniform",
         "incidence" : 0.2
     },
     population=patients.which_exist_in_file(CKD), 
-    index_date="2022-04-01",
+    index_date="2023-04-01",
 
     **variables_additional_v2,
 ) 
